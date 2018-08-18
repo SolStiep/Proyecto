07ec7278de759a7324053ee3dedbe11011c80a7f
SolStiep <solstiep.puan@gmail.com>
2018-08-18 13:26:49 -0300

    Commit 2

--- proyecto/src/proyecto/test.java
+++ proyecto/src/proyecto/test.java
@@ -4,5 +4,11 @@ public class test {
 
 	public static void main(String args[]) {
 		System.out.println("Hello world");
+		prueba(5);
+	}
+	
+	
+	private static void prueba(int a) {
+		System.out.println("El numero es: "+a);
 	}
 }
